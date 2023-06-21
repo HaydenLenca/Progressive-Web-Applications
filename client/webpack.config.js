acfa50d9c9d489d12e8da7dd9ec4e6223a170d7e
HaydenLenca <120521124+HaydenLenca@users.noreply.github.com>
2023-06-21 13:14:09 -0700

    working on install

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = () => {
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128,  82, 256, 384, 512],
-            destination: path.join.apply('assets', 'icons'),
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
