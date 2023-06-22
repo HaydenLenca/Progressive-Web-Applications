97142183393cbba850c7dd1f6c633b2382216c66
HaydenLenca <120521124+HaydenLenca@users.noreply.github.com>
2023-06-22 09:52:58 -0700

    adding readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# Note Editor
+    
+  ### Description
+  This project is to a simple note editor.
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Contribution](#contribution)
+  * [Usage](#usage)
+  * [Links](#links)
+  * [Features](#features)
+  * [License](#license)
+  * [Questions](#questions)
+  
+
+  ### Contribution
+  Starter code for the project by Xandromus https://github.com/coding-boot-camp/cautious-meme
+
+
+  ### Links
+  [Github](https://github.com/HaydenLenca/Progressive-Web-Applications) 
+  
+  [heroku](https://git.heroku.com/the-jate-available.git)
+
+
+  ### License 
+   MIT
+
+  
+   
+
+  ### Questions 
+  Please reach out to me at either Github or email with any questions.
+  #### Github: https://github.com/HaydenLenca
+  #### Contact Email: Hlenca@me.com
--- client/src-sw.js
+++ client/src-sw.js
@@ -24,18 +24,21 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(
+  ({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
-    // Name of the cache storage.
-    cacheName: 'asset-cache',
+  ({ request }) => request.destination === 'src',
+  new CacheFirst({
+    cacheName: 'my-src-cache',
     plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
       }),
     ],
   })
